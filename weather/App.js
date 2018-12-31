6b12e19942d573ecd02fb579708627bb72f53e5b
Nastar Eater <nastarfan@gmail.com>
2018-12-31 22:41:19 +0700

    finalise state logic

--- weather/App.js
+++ weather/App.js
@@ -1,27 +1,56 @@
-import React from 'react';
-import { StyleSheet, Text, View, KeyboardAvoidingView, Platform, TextInput, ImageBackground, } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  KeyboardAvoidingView,
+  Platform,
+  ImageBackground
+} from "react-native";
 
-import SearchInput from './components/SearchInput';
+import SearchInput from "./components/SearchInput";
 
-import getImageForWeather from './utils/getImageForWeather'
+import getImageForWeather from "./utils/getImageForWeather";
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      location: ""
+    };
+  }
+
+  handleUpdateLocation = city => {
+    this.setState({
+      location: city
+    });
+  };
+
+  componentDidMount() {
+    this.handleUpdateLocation("Makassar");
+  }
+
   render() {
+    const { location } = this.state;
     return (
       <KeyboardAvoidingView style={styles.container} behavior="padding">
         <ImageBackground
-          source={getImageForWeather('Snow')}
+          source={getImageForWeather("Snow")}
           style={styles.imageContainer}
           imageStyle={styles.image}
-          >
+        >
           <View style={styles.detailsContainer}>
-            <Text style={[styles.largeText, styles.textStyle]}>San Francisco</Text>
-            <Text style={[styles.smallText, styles.textStyle]}>Light Cloud</Text>
+            <Text style={[styles.largeText, styles.textStyle]}>{location}</Text>
+            <Text style={[styles.smallText, styles.textStyle]}>
+              Light Cloud
+            </Text>
             <Text style={[styles.largeText, styles.textStyle]}>24°</Text>
 
-            <SearchInput placeholder="Search any city" />
+            <SearchInput
+              placeholder="Search any city"
+              onSubmit={this.handleUpdateLocation}
+            />
           </View>
-
         </ImageBackground>
       </KeyboardAvoidingView>
     );
@@ -31,32 +60,32 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff"
   },
   imageContainer: {
-    flex: 1,
+    flex: 1
   },
   image: {
     flex: 1,
     width: null,
     height: null,
-    resizeMode: 'cover',
+    resizeMode: "cover"
   },
   detailsContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(0,0,0,0.2)',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0,0,0,0.2)"
   },
   textStyle: {
-    textAlign: 'center',
-    fontFamily: Platform.OS === 'ios' ? 'AvenirNext-Regular' : 'Roboto',
-    color: 'white',
+    textAlign: "center",
+    fontFamily: Platform.OS === "ios" ? "AvenirNext-Regular" : "Roboto",
+    color: "white"
   },
   largeText: {
-    fontSize: 44,
+    fontSize: 44
   },
   smallText: {
-    fontSize: 18,
-  },
+    fontSize: 18
+  }
 });
--- weather/components/SearchInput.js
+++ weather/components/SearchInput.js
@@ -1,36 +1,61 @@
-import React, { Component } from 'react';
-import {StyleSheet, TextInput, View} from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, TextInput, View } from "react-native";
 
 export default class SearchInput extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ""
+    };
+  }
+
+  handleChangeText = text => {
+    this.setState({ text });
+  };
+
+  handleSubmitEditing = city => {
+    const { onSubmit } = this.props;
+    const { text } = this.state;
+
+    if (!text) return;
+    onSubmit(text);
+    this.setState({ text: "" });
+  };
 
   render() {
+    const { placeholder } = this.props;
+    const { text } = this.state;
+
     return (
       <View style={styles.container}>
         <TextInput
-        autoCorrect={false}
-        placeholder={this.props.placeholder}
-        placeholderTextColor="white"
-        underlineColorAndroid="transparent"
-        style={styles.textInput}
-        clearButtonMode="always" />
+          autoCorrect={false}
+          value={text}
+          placeholder={placeholder}
+          placeholderTextColor="white"
+          underlineColorAndroid="transparent"
+          style={styles.textInput}
+          clearButtonMode="always"
+          onChangeText={this.handleChangeText}
+          onSubmitEditing={this.handleSubmitEditing}
+        />
       </View>
     );
   }
-
 }
 
 const styles = StyleSheet.create({
   container: {
     height: 40,
     marginTop: 20,
-    backgroundColor: '#666',
+    backgroundColor: "#666",
     marginHorizontal: 20,
     paddingHorizontal: 10,
     width: 300,
-    borderRadius: 5,
+    borderRadius: 5
   },
   textInput: {
     flex: 1,
-    color: 'white',
-  },
+    color: "white"
+  }
 });
