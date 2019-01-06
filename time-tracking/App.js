3fd3c9ebb2f70ef921e87355b77247e9e7e7c655
Nastar Eater <nastarfan@gmail.com>
2019-01-07 05:41:02 +0700

    new functional component

--- time-tracking/App.js
+++ time-tracking/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
 
 import EditableTimer from "./components/EditableTimer";
 import ToggleableTimerForm from "./components/ToggleableTimerForm";
@@ -7,18 +7,48 @@ import ToggleableTimerForm from "./components/ToggleableTimerForm";
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.appContainer}>
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>Timer</Text>
+        </View>
+        <ScrollView style={styles.timerList}>
+          <ToggleableTimerForm isOpen={false} />
+          <EditableTimer
+            id="1"
+            title="Mown the lawn"
+            project="Hourse Chores"
+            elapsed="8986300"
+            isRunning
+          />
+          <EditableTimer
+            id="2"
+            title="Bake squash"
+            project="Kitchen Chores"
+            elapsed="3890985"
+            editFormOpen
+          />
+        </ScrollView>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+  },
+  titleContainer: {
+    paddingTop: 35,
+    paddingBottom: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: "#D607DA",
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center'm
+  }
+  timerList: {
+    paddingBottom: 15,
   }
 });
