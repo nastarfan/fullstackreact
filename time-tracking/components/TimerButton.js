93a5495939c8951b0ca3920d7659f74d8858974c
Nastar Eater <nastarfan@gmail.com>
2019-01-08 02:36:35 +0700

    adding form and button components

---
+++ time-tracking/components/TimerButton.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+
+export default function TimerButton({ color, title, small, onPress }) {
+  return (
+    <TouchableOpacity style={[styles.button, { borderColor: color }]}>
+      <Text
+        style={[
+          styles.buttonText,
+          small ? styles.small : styles.large,
+          { color }
+        ]}
+      >
+        {title}
+      </Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    marginTop: 10,
+    minWidth: 100,
+    borderWidth: 2,
+    borderRadius: 3
+  },
+  small: {
+    fontSize: 14,
+    padding: 5
+  },
+  large: {
+    fontSize: 16,
+    padding: 10
+  },
+  buttonText: {
+    textAlign: "center",
+    fontWeight: "bold"
+  }
+});
