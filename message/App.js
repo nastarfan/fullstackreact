32c7db8fc4b649b6aed19556d6b80ad162661fde
Nastar Eater <nastarfan@gmail.com>
2019-02-08 21:19:33 +0700

    statusbar change on network change NetInfo event listener

--- message/App.js
+++ message/App.js
@@ -1,31 +1,29 @@
-import React, {Component} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+import Status from "./components/Status";
 
 export default class App extends Component {
   renderMessageList = () => {
-    return(
-      <View style={styles.content}></View>
-    );
-  }
+    return <View style={styles.content} />;
+  };
 
   renderToolbar = () => {
-    return(
-      <View style={styles.toolbar}></View>
-    );
-  }
+    return <View style={styles.toolbar} />;
+  };
 
   renderInputMethodEditor = () => {
-    return(
-      <View style={styles.inputMethodEditor}></View>
-    );
-  }
-
+    return <View style={styles.inputMethodEditor} />;
+  };
 
   render() {
     return (
-      {this.renderMessageList()}
-      {this.renderToolbar()}
-      {this.renderInputMethodEditor()}
+      <View style={styles.container}>
+        <Status />
+        {this.renderMessageList()}
+        {this.renderToolbar()}
+        {this.renderInputMethodEditor()}
+      </View>
     );
   }
 }
@@ -33,19 +31,19 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white'
+    backgroundColor: "white"
   },
   content: {
-    flex:1,
-    backgroundColor: 'white'
+    flex: 1,
+    backgroundColor: "white"
   },
   inputMethodEditor: {
     flex: 1,
-    backgroundColor: 'white'
+    backgroundColor: "white"
   },
   toolbar: {
     borderTopWidth: 1,
-    borderTopColor: 'rgba(0,0,0,0.4)',
-    backgroundColor: 'white',
+    borderTopColor: "rgba(0,0,0,0.04)",
+    backgroundColor: "white"
   }
 });
---
+++ message/components/Status.js
@@ -0,0 +1,98 @@
+import React, { Component } from "react";
+import {
+  View,
+  StyleSheet,
+  Text,
+  NetInfo,
+  Platform,
+  StatusBar
+} from "react-native";
+import { Constants } from "expo";
+
+const statusHeight = Platform.OS === "ios" ? Constants.statusBarHeight : 0;
+
+export default class Status extends Component {
+  state = {
+    info: "null"
+  };
+
+  async componentWillMount() {
+    this.subscription = NetInfo.addEventListener(
+      "connectionChange",
+      this.handleChange
+    );
+
+    const info = await NetInfo.getConnectionInfo();
+
+    this.setState({ info });
+
+    setTimeout(() => this.handleChange("none"), 3000);
+  }
+
+  componentWillUnmount() {
+    this.subscription.remove();
+  }
+
+  handleChange = info => {
+    this.setState({ info });
+  };
+
+  render() {
+    const { info } = this.state;
+
+    const isConnected = info !== "none";
+    const backgroundColor = isConnected ? "white" : "red";
+
+    const statusBar = (
+      <StatusBar
+        backgroundColor={backgroundColor}
+        barStyle={isConnected ? "dark-content" : "light-content"}
+        animated={false}
+      />
+    );
+
+    const messageContainer = (
+      <View style={styles.messageContainer} pointerEvents={"none"}>
+        {statusBar}
+        {!isConnected && (
+          <View style={styles.bubble}>
+            <Text style={styles.text}>No network connection</Text>
+          </View>
+        )}
+      </View>
+    );
+
+    if (Platform.OS === "ios") {
+      return (
+        <View style={[styles.status, { backgroundColor }]}>{statusBar}</View>
+      );
+    }
+
+    return messageContainer;
+  }
+}
+
+const styles = StyleSheet.create({
+  status: {
+    zIndex: 1,
+    height: statusHeight
+  },
+  messageContainer: {
+    zIndex: 1,
+    position: "absolute",
+    top: statusHeight + 20,
+    right: 0,
+    left: 0,
+    height: 80,
+    alignItems: "center"
+  },
+  bubble: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 20,
+    backgroundColor: "red"
+  },
+  text: {
+    color: "white"
+  }
+});
