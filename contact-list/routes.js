9d5c82bb1e79cf023ef383aaac8230ebd88a2646
nastarfan <nastarfan@gmail.com>
2019-03-31 21:55:11 +0700

    adding bottomTabNavigator

--- contact-list/routes.js
+++ contact-list/routes.js
@@ -1,27 +1,101 @@
-import { createStackNavigator, createAppContainer } from 'react-navigation';
+import React, { Component } from 'react';
+import {
+  createBottomTabNavigator,
+  createStackNavigator,
+  createAppContainer,
+} from 'react-navigation';
+import { MaterialIcons } from '@expo/vector-icons';
 
 import Contacts from './screens/Contacts';
 import Profile from './screens/Profile';
 import Favorites from './screens/Favorites';
 import User from './screens/User';
 
-const AppNavigator = createStackNavigator(
+import colors from './utils/colors';
+
+const getTabBarIcon = icon => ({ tintColor }) => (
+  <MaterialIcons name={icon} size={26} style={{ color: tintColor }} />
+);
+
+const ContactsScreens = createAppContainer(
+  createStackNavigator(
+    {
+      Contacts: {
+        screen: Contacts,
+      },
+      Profile: {
+        screen: Profile,
+      },
+    },
+    {
+      initialRouteName: 'Contacts',
+      navigationOptions: {
+        tabBarIcon: getTabBarIcon('list'),
+      },
+    },
+  ),
+);
+
+const FavoritesScreens = createAppContainer(
+  createStackNavigator(
+    {
+      Contacts: {
+        screen: Favorites,
+      },
+      Profile: {
+        screen: Profile,
+      },
+    },
+    {
+      initialRouteName: 'Favorites',
+      navigationOptions: {
+        tabBarIcon: getTabBarIcon('star'),
+      },
+    },
+  ),
+);
+
+const UserScreens = createAppContainer(
+  createStackNavigator(
+    {
+      User: {
+        screen: User,
+      },
+    },
+    {
+      initialRouteName: 'User',
+      navigationOptions: {
+        tabBarIcon: getTabBarIcon('person'),
+      },
+    },
+  ),
+);
+
+const AppNavigator = createBottomTabNavigator(
   {
     Contacts: {
-      screen: Contacts,
-    },
-    Profile: {
-      screen: Profile,
+      screen: ContactsScreens,
     },
     Favorites: {
-      screen: Favorites,
+      screen: FavoritesScreens,
     },
     User: {
-      screen: User,
+      screen: UserScreens,
     },
   },
   {
-    initialRouteName: 'User',
+    initialRouteName: 'Contacts',
+    tabBarPosition: 'bottom',
+    tabBarOptions: {
+      style: {
+        backgroundColor: colors.greyLight,
+      },
+      showLabel: false,
+      showIcon: true,
+      activeTintColor: colors.blue,
+      inactiveTintColor: colors.greyDark,
+      renderIndicator: () => null,
+    },
   },
 );
 
